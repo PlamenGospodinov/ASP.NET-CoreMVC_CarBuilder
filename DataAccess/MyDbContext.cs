286633436d26754cc305a54c88c45eebef349c6c
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-03-04 20:09:12 +0200

    Add files via upload

---
+++ DataAccess/MyDbContext.cs
@@ -0,0 +1,29 @@
+﻿using CarBuilder.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarBuilder.DataAccess
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+        
+        public DbSet<Client> Clients { get; set; }
+
+        public DbSet<Garagee> Garages { get; set; }
+        public MyDbContext()
+        {
+            Cars = this.Set<Car>();
+            Clients = this.Set<Client>();
+            Garages = this.Set<Garagee>();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=CarsDb;User Id=gospodinovp;Password=1234;");
+        }
+    }
+}
